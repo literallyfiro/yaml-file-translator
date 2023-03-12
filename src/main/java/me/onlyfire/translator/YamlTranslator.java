795eda8d4fa0547d2222dde6b7b1c935006acb35
ImOnlyFire <ivancardaropoli77@gmail.com>
2023-03-12 20:19:45 +0100

    Add key counter when translating

--- src/main/java/me/onlyfire/translator/YamlTranslator.java
+++ src/main/java/me/onlyfire/translator/YamlTranslator.java
@@ -4,8 +4,8 @@ import com.deepl.api.DeepLException;
 import com.deepl.api.TextResult;
 import com.deepl.api.Translator;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class YamlTranslator {
@@ -16,37 +16,61 @@ public class YamlTranslator {
         this.translator = translator;
     }
 
-    @SuppressWarnings("unchecked")
     public void translateMap(Map<String, Object> map, String lang_to_translate) {
+        translateMap(map, lang_to_translate, new AtomicInteger(countKeys(map)), "");
+    }
+
+    @SuppressWarnings("unchecked")
+    private void translateMap(Map<String, Object> map, String lang_to_translate, AtomicInteger countKeys, String currentKeyPath) {
         for (Map.Entry<String, Object> entry : map.entrySet()) {
             try {
+                String fullKeyPath = currentKeyPath.isEmpty() ? entry.getKey() : currentKeyPath + "." + entry.getKey();
+                System.out.println("Translating key: " + fullKeyPath + " (Remaining: " + countKeys.decrementAndGet() + " keys)");
+
                 if (entry.getValue() instanceof String) {
                     var translated = translateString((String) entry.getValue(), lang_to_translate);
                     map.put(entry.getKey(), translated);
                 } else if (entry.getValue() instanceof Map) {
                     // value is a map, call translateMap recursively
                     Map<String, Object> innerMap = (Map<String, Object>) entry.getValue();
-                    translateMap(innerMap, lang_to_translate);
+                    translateMap(innerMap, lang_to_translate, countKeys, fullKeyPath);
                 } else if (entry.getValue() instanceof List<?> && ((List<?>) entry.getValue()).get(0) instanceof String) {
                     var translated = translateList((List<String>) entry.getValue(), lang_to_translate);
                     map.put(entry.getKey(), translated);
                 }
+
             } catch (InterruptedException | DeepLException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
-    public List<String> translateList(List<String> list, String lang_to_translate)
+    private List<String> translateList(List<String> list, String lang_to_translate)
             throws DeepLException, InterruptedException {
         var translated_list = translator.translateText(list, null, lang_to_translate);
         return translated_list.stream().map(TextResult::getText).collect(Collectors.toList());
     }
 
-    public String translateString(String string, String lang_to_translate)
+    private String translateString(String string, String lang_to_translate)
             throws DeepLException, InterruptedException {
         if (string.isEmpty()) return string;
         return translator.translateText(string, null, lang_to_translate).getText();
+    }
+
+    private int countKeys(Map<String, Object> map) {
+        int count = map.size();
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            if (entry.getValue() instanceof Map) {
+                count += countKeys((Map<String, Object>) entry.getValue());
+            } else if (entry.getValue() instanceof List) {
+                for (Object item : (List) entry.getValue()) {
+                    if (item instanceof Map) {
+                        count += countKeys((Map<String, Object>) item);
+                    }
+                }
+            }
+        }
+        return count;
     }
 
 }
