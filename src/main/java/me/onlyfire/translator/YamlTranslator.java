74be5dfdb6a244dc94138277e6289757f4514435
ImOnlyFire <ivancardaropoli77@gmail.com>
2023-03-29 20:56:18 +0200

    Code refactor

--- src/main/java/me/onlyfire/translator/YamlTranslator.java
+++ src/main/java/me/onlyfire/translator/YamlTranslator.java
@@ -21,21 +21,21 @@ public class YamlTranslator {
     }
 
     @SuppressWarnings("unchecked")
-    private void translateMap(Map<String, Object> map, String lang_to_translate, AtomicInteger countKeys, String currentKeyPath) {
+    private void translateMap(Map<String, Object> map, String langToTranslate, AtomicInteger countKeys, String currentKeyPath) {
         for (Map.Entry<String, Object> entry : map.entrySet()) {
             try {
                 String fullKeyPath = currentKeyPath.isEmpty() ? entry.getKey() : currentKeyPath + "." + entry.getKey();
                 System.out.println("Translating key: " + fullKeyPath + " (Remaining: " + countKeys.decrementAndGet() + " keys)");
 
                 if (entry.getValue() instanceof String) {
-                    var translated = translateString((String) entry.getValue(), lang_to_translate);
+                    var translated = translateString((String) entry.getValue(), langToTranslate);
                     map.put(entry.getKey(), translated);
                 } else if (entry.getValue() instanceof Map) {
                     // value is a map, call translateMap recursively
                     Map<String, Object> innerMap = (Map<String, Object>) entry.getValue();
-                    translateMap(innerMap, lang_to_translate, countKeys, fullKeyPath);
+                    translateMap(innerMap, langToTranslate, countKeys, fullKeyPath);
                 } else if (entry.getValue() instanceof List<?> && ((List<?>) entry.getValue()).get(0) instanceof String) {
-                    var translated = translateList((List<String>) entry.getValue(), lang_to_translate);
+                    var translated = translateList((List<String>) entry.getValue(), langToTranslate);
                     map.put(entry.getKey(), translated);
                 }
 
@@ -45,16 +45,16 @@ public class YamlTranslator {
         }
     }
 
-    private List<String> translateList(List<String> list, String lang_to_translate)
+    private List<String> translateList(List<String> list, String langToTranslate)
             throws DeepLException, InterruptedException {
-        var translated_list = translator.translateText(list, null, lang_to_translate);
+        var translated_list = translator.translateText(list, null, langToTranslate);
         return translated_list.stream().map(TextResult::getText).collect(Collectors.toList());
     }
 
-    private String translateString(String string, String lang_to_translate)
+    private String translateString(String string, String langToTranslate)
             throws DeepLException, InterruptedException {
         if (string.isEmpty()) return string;
-        return translator.translateText(string, null, lang_to_translate).getText();
+        return translator.translateText(string, null, langToTranslate).getText();
     }
 
     private int countKeys(Map<String, Object> map) {
